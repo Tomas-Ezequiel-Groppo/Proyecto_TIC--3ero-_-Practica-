f01d9e6f84de2cd1d827480002223bcd43d59a9e
GROPPO TOMAS EZEQUIEL <50976178@ort>
2026-08-25 15:53:13 -0300

    Inicio del documento index.ts

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+type Profile = {username:string ; password:string ; profileID:number};
+
+let perfil1 : Profile = {
+    username: "xXJuanelcrackXx",
+    password : "odioatodos",
+    profileID : 12,
+}
+
+let perfil2 : Profile = {
+    username: "Miguelprogamer235215",
+    password : "The_special_one",
+    profileID : 23,
+}
+
+let perfiles : Profile[] = [];
+
+let n : string = "hola"
+let m : string = "HolaMundo"
+let o : number = 24
+
+perfiles.push(perfil1)
+
+let perfil3 : Profile = {
+    username: n,
+    password: m,
+    profileID: o,
+}
+
