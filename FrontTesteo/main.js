d8a7ebe90d63127f0d7aeb483f5b6d5e88e83a6e
Tomas-Ezequiel-Groppo <tommygroppo70@gmail.com>
2026-09-09 20:43:21 -0300

    Empezando con las app, buscando entender el traspaso de archivos

---
+++ FrontTesteo/main.js
@@ -0,0 +1,33 @@
+alert("HolaMundo");
+import express from 'express'
+const app= express()
+const puerto=5237;
+
+app.use(express.json());
+
+app.listen(puerto, () =>{
+    console.log("Todo bien. El testeo está en posición de arrancar");
+});
+
+let profileID=0;
+
+button.addEventListener("click",() => {
+    if(username.value="pepito"){
+        profileID=30;
+        alert("Hola Pepito");
+    }
+    else if (username.value="juansito"){
+        profileID=31;
+        alert("Hola Juansito");
+    }
+    else if (username.value="santino"){
+         profileID=32;
+         alert("Hola Santino");
+    }
+    else{
+        profileID=0;
+        alert("¿Como hiciste para romper un multiple-choice");
+    }
+})
+
+
