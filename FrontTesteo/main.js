9ef5fe026c6c91ba3403579e55fe70f366555599
GROPPO TOMAS EZEQUIEL <50976178@ort>
2026-09-10 12:08:24 -0300

    Sigo sin entender las app(El fin de semana va a ser tristisimo)

--- FrontTesteo/main.js
+++ FrontTesteo/main.js
@@ -1,33 +1,19 @@
-alert("HolaMundo");
 import express from 'express'
 const app= express()
 const puerto=5237;
-
+import { readFile } from 'fs/promises'
 app.use(express.json());
+
+app.get("/", (req, res) =>{
+    readFile(new URL('../src/main.json', import.meta.url), 'utf-8')
+        .then(data => res.json(JSON.parse(data)))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: "No se pudo leer el archivo" });
+        });
+
+})
 
 app.listen(puerto, () =>{
     console.log("Todo bien. El testeo está en posición de arrancar");
 });
-
-let profileID=0;
-
-button.addEventListener("click",() => {
-    if(username.value="pepito"){
-        profileID=30;
-        alert("Hola Pepito");
-    }
-    else if (username.value="juansito"){
-        profileID=31;
-        alert("Hola Juansito");
-    }
-    else if (username.value="santino"){
-         profileID=32;
-         alert("Hola Santino");
-    }
-    else{
-        profileID=0;
-        alert("¿Como hiciste para romper un multiple-choice");
-    }
-})
-
-
